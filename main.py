44159ef609a667ddbf8abe3a2fe4e1f3194a9722
Eduardo <eduardo_lopes010@hotmail.com>
2025-12-04 11:18:39 -0300

    testando tkinter

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello World')
+import tkinter as tk
+
+janela = tk.Tk()
+janela.title('Tela de Login')
+janela.geometry('400x300')
+
+texto = tk.Label(janela, text=f'Bem vindo')
+texto.pack()
+
+def resposta_botao():
+    nome = entrada_texto.get()
+    texto['text'] = f'Olá, {nome}!'
+
+entrada_texto = tk.Entry(janela)
+entrada_texto.pack()
+
+tk.Button(janela, text='Clique aqui', command=resposta_botao).pack()
+
+janela.mainloop()
